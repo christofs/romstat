4cc4b94e99f032adea031beea74de9f72b7efe82
christofs <c.schoech@gmail.com>
2021-09-16 16:10:06 +0200

    stellen nach sprachen

--- code/viz-alle-sprache.py
+++ code/viz-alle-sprache.py
@@ -23,24 +23,19 @@ def get_data():
 def prepare_data(data): 
 	# Filter down to useable data
 	data = data.fillna(0)
-	data = data.loc[:,["include", "domain_lit", "domain_ling", "domain_mkw", "domain_fdid", "domain_other"]]
+	data = data.loc[:,["include", "lang_string"]]
 	data = data[data["include"] == 1]
 	#print(data.head())
 	n = data.shape[0]
 	print("Anzahl der Datenpunkte", n)
 	from collections import Counter
-	data = {"domain_lit":sum(data["domain_lit"]),
-			"domain_ling":sum(data["domain_ling"]),
-			"domain_mkw":sum(data["domain_mkw"]),
-			"domain_fdid":sum(data["domain_fdid"]),
-			"domain_other":sum(data["domain_other"]),
-			}
+	data = dict(Counter(list(data.loc[:,"lang_string"])))
 	print(data)
 	return data,n
 
 
 def viz(data,n): 
-	dark_lighten_style = LightenStyle('#004466',
+	dark_lighten_style = LightenStyle('#063d1e',
 		step=10, 
 		font_family="FreeSans",
 		label_font_size = 12,
@@ -55,14 +50,17 @@ def viz(data,n):
     	legend_at_bottom = False,
 		legend_at_bottom_columns = 8,
 		legend_box_size=32)
-	chart.title = "Ausschreibungen nach Fachgebieten"
-	chart.x_title = "Anteile der Fachgebiete in Prozent (Mehrfachnennungen möglich; n="+str(n)+")"
-	chart.add("domain_lit", data["domain_lit"]/n*100, formatter=lambda x: 'Literaturwiss.: {:.1f}%'.format(x))
-	chart.add("domain_ling", data["domain_ling"]/n*100, formatter=lambda x: 'Sprachwiss.: {:.1f}%'.format(x))
-	chart.add("domain_mkw", data["domain_mkw"]/n*100, formatter=lambda x: 'Medien- und Kulturwiss.: {:.1f}%'.format(x))
-	chart.add("domain_fdid", data["domain_fdid"]/n*100, formatter=lambda x: 'Fachdidaktik {:.1f}%'.format(x))
-	chart.add("domain_other", data["domain_other"]/n*100, formatter=lambda x: 'Weitere Bereiche {:.1f}%'.format(x))
-	chart.render_to_file("../img/romanistik_alle-fachgebiete.svg")
+	chart.title = "Sprachen"
+	chart.x_title = "Anteile der genannten Sprachen in Prozent (n="+str(n)+")"
+	chart.y_title = "Sprachen"
+	chart.x_labels = ["sprachüb.", "mehrere", "Port.", "Ital.", "Span.", "Franz."]
+	chart.add("Sprachen", [data["other"]/n*100,
+							   data["mult"]/n*100,
+							   data["por"]/n*100,
+							   data["ita"]/n*100,
+							   data["spa"]/n*100,
+							   data["frz"]/n*100], formatter=lambda x: '{:.1f}%'.format(x))
+	chart.render_to_file("../img/romanistik_alle-sprachen.svg")
 			
 			
 
