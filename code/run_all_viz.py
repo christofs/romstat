0d9c0eafab8be51d597e853f9a003ab7a192f9ad
christofs <c.schoech@gmail.com>
2021-09-25 09:02:05 +0200

    mehr sws-daten; update im text zu sws; run_all

---
+++ code/run_all_viz.py
@@ -0,0 +1,61 @@
+import viz_alle_eingruppierung
+import viz_alle_fachgebiete
+import viz_alle_jahre
+import viz_alle_laufzeit
+import viz_alle_sprache
+import viz_alle_stellentyp
+import viz_alle_sws
+import viz_alle_umfang
+import viz_laufzeit_fachgebiete_synopse
+import viz_laufzeit_fachgebiet_fdid
+import viz_laufzeit_fachgebiet_ling
+import viz_laufzeit_fachgebiet_lit
+import viz_laufzeit_fachgebiet_mkw
+import viz_laufzeit_fachgebiet_other
+import viz_laufzeit_fachgebiet_spr
+import viz_laufzeit_sprache
+import viz_laufzeit_sprache_synopse
+import viz_laufzeit_stellentyp_lektorat
+import viz_laufzeit_stellentyp_lfba
+import viz_laufzeit_stellentyp_rat
+import viz_laufzeit_stellentyp_synopse
+import viz_laufzeit_stellentyp_wma
+import viz_lehrverpflichtung_stellentyp_lektorat
+import viz_lehrverpflichtung_stellentyp_lfba
+import viz_lehrverpflichtung_stellentyp_rat
+import viz_lehrverpflichtung_stellentyp_synopse
+import viz_lehrverpflichtung_stellentyp_wma
+import viz_unbefristet_stellentyp
+
+
+
+#viz_alle_eingruppierung.main()
+#viz_alle_fachgebiete.main()
+#viz_alle_jahre.main()
+#viz_alle_laufzeit.main()
+#viz_alle_laufzeit_jahre.main()
+#viz_alle_sprache.main()
+#viz_alle_stellentyp.main()
+#viz_alle_sws.main()
+#viz_alle_umfang.main()
+#viz_laufzeit_fachgebiete_synopse.main()
+#viz_laufzeit_fachgebiet_fdid.main()
+#viz_laufzeit_fachgebiet_ling.main()
+#viz_laufzeit_fachgebiet_lit.main()
+#viz_laufzeit_fachgebiet_mkw.main()
+#viz_laufzeit_fachgebiet_other.main()
+#viz_laufzeit_fachgebiet_spr.main()
+#viz_laufzeit_sprache.main()
+#viz_laufzeit_sprache_synopse.main()
+#viz_laufzeit_stellentyp_lektorat.main()
+#viz_laufzeit_stellentyp_lfba.main()
+#viz_laufzeit_stellentyp_rat.main()
+#viz_laufzeit_stellentyp_synopse.main()
+#viz_laufzeit_stellentyp_wma.main()
+#viz_lehrverpflichtung_stellentyp_lektorat.main()
+#viz_lehrverpflichtung_stellentyp_lfba.main()
+#viz_lehrverpflichtung_stellentyp_rat.main()
+#viz_lehrverpflichtung_stellentyp_synopse.main()
+#viz_lehrverpflichtung_stellentyp_wma.main()
+#viz_unbefristet_stellentyp.main()
+
--- code/viz_laufzeit_jahre.py
+++ code/viz_laufzeit_jahre.py
@@ -13,38 +13,23 @@ from collections import Counter
 
 
 def get_data(): 
-	with open("../data/romanistik-stellen-datensatz_2021-09-09.csv", "r", encoding="utf8") as infile: 
+	with open("../data/romanistik-stellen_datensatz_2014-2021.csv", "r", encoding="utf8") as infile: 
 		data = pd.read_csv(infile, sep="\t")
 		print(data.head())
 		return data
 
 
 
-def viz_dauer(data): 
+def prepare(data): 
 	# Filter down to useable data
 	dauerdata = data.loc[:,["jahr", "dauer_norm", "include"]]
-	dauerdata = dauerdata[dauerdata["dauer_norm"].apply(lambda x: str(x).isdigit())]
-	dauerdata = dauerdata[dauerdata["jahr"].apply(lambda x: str(x).isdigit())]
+	#dauerdata = dauerdata[dauerdata["dauer_norm"].apply(lambda x: str(x).isdigit())]
+	#dauerdata = dauerdata[dauerdata["jahr"].apply(lambda x: str(x).isdigit())]
 	dauerdata = dauerdata[dauerdata["include"] == 1]
 	#print(dauerdata.head())
-	datenpunktesumme = dauerdata.shape[0]
-	print("Anzahl der Datenpunkte", datenpunktesumme)
+	n = dauerdata.shape[0]
+	print("Anzahl der Datenpunkte", n)
 	dauerdata = dauerdata.groupby("jahr")
-	
-	# Make boxplot (distribution)
-	boxplot = pygal.Box(box_mode="tukey", 
-					    style=CleanStyle,
-					    legend_at_bottom = True,
-					    legend_at_bottom_columns = 8,
-					    truncate_legend=5)
-	boxplot.title = "Vertragsdauer pro Jahr\n(Daten von romanistik.de, 03/2014-07/2021, Stellen: "+str(datenpunktesumme)+")"
-	boxplot.x_title = "Jahre"
-	boxplot.y_title = "Vertragsdauer in Monaten\n(120=unbefristet)"
-	for year,group in dauerdata:
-		dauerdatayear = list(group["dauer_norm"].astype(int)) 
-		label = str(year) + " (" + str(len(dauerdatayear)) + " Stellen)"
-		boxplot.add(label, dauerdatayear)
-	boxplot.render_to_file("romanistik_jahr-dauer_boxplot.svg")
 	
 	# Prepare data for stacked barchart
 	yearlypercentages = {}
@@ -52,7 +37,7 @@ def viz_dauer(data):
 	categories = ["1-6", "~12", "~24", "~36", "~48", "~60", "~72", "78+", "unb."]
 	yearlypercentages["cats"] = categories
 	for year,group in dauerdata: 
-		dauerdatayear = list(group["dauer_norm"].astype(int)) 
+		dauerdatayear = list(group["dauer_norm"])#.astype(int)) 
 		numitems = len(dauerdatayear)
 		label = str(year) + " (" + str(numitems) + " Stellen)"
 		#print(label)
@@ -96,20 +81,23 @@ def viz_dauer(data):
 	yearlypercentages.set_index("cats", inplace=True)
 	yearlypercentages = yearlypercentages.T
 	#print(yearlypercentages)
+	data = yearlypercentages
+	return data, n
 
+def make_viz(data, n): 
 	# Make stacked barchart (proportions)
 	barchart = pygal.StackedBar(style=BlueStyle,
     				     legend_at_bottom = False,
 					     legend_at_bottom_columns = 5,
 					     legend_box_size=24,
 					     truncate_legend=5)
-	barchart.title = "Anteile der Vertragsdauern pro Jahr (n="+str(datenpunktesumme)+")"
+	barchart.title = "Anteile der Vertragsdauern pro Jahr (n="+str(n)+")"
 	barchart.x_title = "Jahre"
 	barchart.y_title = "Anteile (Prozent) der Gruppen von Vertragsdauern"
 	barchart.x_labels = map(str, range(2014, 2022))
-	for row in yearlypercentages: 
-		label = yearlypercentages[row].name
-		data = list(yearlypercentages[row])
+	for row in data: 
+		label = data[row].name
+		data = list(data[row])
 		#print(label, data)		
 		barchart.add(label, data, formatter=lambda x: '{:.1f}%'.format(x))
 	barchart.render_to_file("../img/romanistik_jahr-dauer.svg")
@@ -119,7 +107,8 @@ def viz_dauer(data):
 
 def main(): 
 	data = get_data()
-	viz_dauer(data)
+	data,n = prepare(data)
+	make_viz(data,n)
 	
 
 main()	
